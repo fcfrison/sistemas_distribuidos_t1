1f387de2a4462e11a0d94f185ba81586dedce4ca
Felipe Frison <fcfrison@gmail.com>
2025-03-27 10:02:56 -0300

    feat(dimex): implement the handle_upon_req_entry function.

--- include/DIMEX.h
+++ include/DIMEX.h
@@ -1,10 +1,11 @@
+#include <semaphore.h>
 #include "../include/chan.h"
 #include "../include/p2plink.h"
 #define HANDLE_NULL_ALLOC(ptr, cleanup_label) \
     if (!(ptr)) { \
         goto cleanup_label; \
     }
-#define FREE_MEMORY(ptr, cleanup_label) \
+#define CLEANUP(cleanup_label) \
     goto cleanup_label; 
 
 typedef enum{
@@ -28,10 +29,11 @@ typedef struct{
     unsigned char addresses_len;
     unsigned int id;
     State        st;
-    char*        waiting;
+    char**       waiting;
     int          lcl;
     int          req_ts;
     int          nbr_resps;
     PP2PLink*    p2p;
+    sem_t*       global_mutex;
 }DIMEX_Module;
 
--- src/DIMEX.c
+++ src/DIMEX.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <semaphore.h>
 #include "../include/chan.h"
 #include "../include/p2plink.h"
 #include "../include/DIMEX.h"
@@ -15,22 +16,25 @@ __new_dimex(char** addresses, unsigned char addresses_len, int id){
     chan*     Req = start_chan(1, (void**)calloc(1, sizeof(dmxReq*)));
     chan*     Ind = start_chan(1, (void**)calloc(1, sizeof(dmxResp*)));
     char** waiting = (char**)calloc(addresses_len,sizeof(char*));
+    sem_t* global_mutex = (sem_t*)calloc(1,sizeof(sem_t));
+    sem_init(global_mutex,0,1);
     for (size_t i = 0; i < addresses_len; i++){
         waiting[i] = calloc(1,sizeof(char));
         *waiting[i] = 0;
     };
     DIMEX_Module* dimex = (DIMEX_Module*)calloc(1,sizeof(DIMEX_Module));
-    dimex->Req       = Req;
-    dimex->Ind       = Ind;
-    dimex->addresses = addresses;
+    dimex->Req           = Req;
+    dimex->Ind           = Ind;
+    dimex->addresses     = addresses;
     dimex->addresses_len = addresses_len;
-    dimex->id        = id;
-    dimex->st        = noMX;
-    dimex->lcl       = 0;
-    dimex->req_ts    = 0;
-    dimex->nbr_resps = 0;
-    dimex->waiting   = waiting;
-    dimex->p2p       = p2p;
+    dimex->id            = id;
+    dimex->st            = noMX;
+    dimex->lcl           = 0;
+    dimex->req_ts        = 0;
+    dimex->nbr_resps     = 0;
+    dimex->waiting       = waiting;
+    dimex->p2p           = p2p;
+    dimex->global_mutex  = global_mutex;
     return dimex;
 };
 DIMEX_Module*
@@ -43,6 +47,7 @@ void*
 handle_msg_from_app(DIMEX_Module* module){
     while(1){
         dmxReq* dmxR = receive_data(module->Req);
+        sem_wait(module->global_mutex);
         switch (*dmxR){
             case ENTER:
                 printf("app request mutex - process id%d\n",module->id);
@@ -55,6 +60,7 @@ handle_msg_from_app(DIMEX_Module* module){
         default:
             break;
         };
+        sem_post(module->global_mutex);
         if(dmxR){
             free(dmxR);
         };
@@ -72,13 +78,15 @@ while (1){
     };
     HANDLE_NULL_ALLOC(msg_outro->from,cleanup_msg_ptr);
     HANDLE_NULL_ALLOC(msg_outro->message,cleanup_msg_ptr);
+    sem_wait(module->global_mutex);
     if(strstr(msg_outro->message,"respOK")){
         //handleUponDeliverRespOk
     }else if(strstr(msg_outro->message,"reqEntry")){
         //handleUponDeliverReqEntry
     }else{
-        FREE_MEMORY(msg_outro,free_mem);
+        CLEANUP(free_mem);
     }
+    sem_post(module->global_mutex);
     };
     cleanup_msg_ptr:
         if(msg_outro->message) free(msg_outro->message);
@@ -96,23 +104,53 @@ start_dimex(DIMEX_Module* module){
     pthread_t* handle_peer_msg_th = NULL;
     handle_app_th = (pthread_t*)calloc(1,sizeof(pthread_t));
     handle_peer_msg_th = (pthread_t*)calloc(1,sizeof(pthread_t));
-    HANDLE_NULL_ALLOC(handle_app_th, cleanup_calloc_error);
-    HANDLE_NULL_ALLOC(handle_peer_msg_th, cleanup_calloc_error);
+    HANDLE_NULL_ALLOC(handle_app_th, cleanup_start_dimex);
+    HANDLE_NULL_ALLOC(handle_peer_msg_th, cleanup_start_dimex);
     if(pthread_create(handle_app_th, NULL, handle_msg_from_app, module)!=0){
-        if(handle_app_th) free(handle_app_th);
-        fprintf(stderr, "Error: failed to create thread\n");
-        exit(1);
+        CLEANUP(cleanup_start_dimex);
     };
     // another to handle messages coming from other processes
     if(pthread_create(handle_peer_msg_th, NULL, handle_peer_msg, module)!=0){
-        if(handle_peer_msg_th) free(handle_peer_msg_th);
-        fprintf(stderr, "Error: failed to create thread\n");
-        exit(1);
+        CLEANUP(cleanup_start_dimex);
     };
-    cleanup_calloc_error:
+    cleanup_start_dimex:
         if(handle_app_th) free(handle_app_th);
         if(handle_peer_msg_th) free(handle_peer_msg_th);
-        fprintf(stderr, "Error: failed to allocate memory\n");
+        fprintf(stderr, "Error: failed to start dimex\n");
         exit(1);
-}
+};
 
+void
+send_to_link(DIMEX_Module* dimex, char* address, char* content){
+    PP2PLink_Req_Message* msg = init_p2p_req(address, content);
+    send_data(dimex->p2p->req, msg);
+    printf("Warning: message sent from process %d to %s\n", dimex->id, content);
+};
+
+void
+handle_upon_req_entry(DIMEX_Module* module){
+    module->lcl++;
+    module->req_ts    = module->lcl;
+    module->nbr_resps = 0;
+    char* message     = NULL;
+    char* entry_msg   = "reqEntry";
+    unsigned int entry_msg_len = strlen(entry_msg);
+    char* pipe        = "|";
+    int   pipe_len    = 1;
+    char  id_str[6]   = {0};
+    sprintf(id_str, "%d", module->id);
+    unsigned int id_str_len = strlen(id_str);
+    char req_ts_str[20] = {0};
+    sprintf(req_ts_str, "%d", module->req_ts);
+    char req_ts_str_len = strlen(req_ts_str);
+    int msg_len = entry_msg_len + pipe_len + id_str_len + pipe_len + req_ts_str_len + 1;
+    for (size_t i = 0; i < module->addresses_len; i++){
+        if(i!=module->id){
+            char* message = (char*) calloc(msg_len,sizeof(char));
+            sprintf(message, "%s|%s|%s", entry_msg, id_str, req_ts_str);
+            send_to_link(module,module->addresses[i],message);
+        };
+    };
+    module->st = wantMX;
+    return;
+}
